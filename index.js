cdc5fe45d349aff2acc5232cbb61409d89683283
Rolin Blake <rolinmblake@gmail.com>
2023-06-22 18:15:44 -0500

    separated javascript into separate file index.js; added user inputs to play with parameters and some guidelines/warnings when user enters invalid params

---
+++ index.js
@@ -0,0 +1,100 @@
+document.getElementById("gen-button").addEventListener("click", () => {
+	var canvas = document.createElement("canvas");
+	canvas.width = 1500;
+	canvas.height = 1500;
+	var context = canvas.getContext("2d");
+	var frequency = document.getElementById("frequency-input").value;
+	if (frequency <= 0) {
+		alert("Please enter a number for frequency greater than 0 and less than or equal to 500.");
+		return;
+	}
+	if (frequency > 500) {
+		alert("Please enter a number for frequency greater than 0 and less than or equal to 500.");
+		return;
+	}
+	var amplitude = document.getElementById("amplitude-input").value;
+	if (amplitude <= 0) {
+		alert("Please enter an amplitude greater than 0.");
+		return;
+	}
+	if (amplitude > 500) {
+		alert("Please enter a number for amplitude greater than 0 and less than or equal to 500.");
+		return;
+	}
+	var circle_num = document.getElementById("circle-input").value;
+	if (circle_num <= 0) {
+		alert("Please enter a positive integer for cirlce iterations greater than or equal to 1 and less than or equal to 500.");
+		return;
+	}
+	if (!Number.isInteger(parseFloat(circle_num))) {
+		alert("Please enter a positive integer number for circle generation iterations.");
+		return;
+	}
+	if (circle_num > 500) {
+		alert("Please enter a positive integer for cirlce iterations greater than or equal to 1 and less than or equal to 500.");
+		return;
+	}
+	var warp_scale = document.getElementById("warp-input").value;
+	if (warp_scale <= 0) {
+		alert("Please enter a number for warp scaling greater than 0 and less than or equal to 500.");
+		return;
+	}
+	if (warp_scale > 500) {
+		alert("Please enter a number for warp scaling greater than 0 and less than or equal to 500.");
+		return;
+	}
+	
+	// Generate the trippy grid pattern
+	for (let x = 0; x < canvas.width; x++) {
+        for (let y = 0; y < canvas.height; y++) {
+			let r = Math.sin(x * frequency) * amplitude;
+			let g = Math.sin(y * frequency) * amplitude;
+			let b = Math.sin((x + y)  * frequency) * amplitude;
+			let color = 'rgb(' + Math.floor(r) + ', ' + Math.floor(g) + ', ' + Math.floor(b) + ')';
+            context.fillStyle = color;
+            context.fillRect(x, y, 1, 1);
+		}
+	}
+	// Add fractal circles
+    context.globalCompositeOperation = 'screen';
+    for (let i = 0; i < circle_num; i++) {
+		let startX = Math.random() * canvas.width;
+		let startY = Math.random() * canvas.height;
+		let radius = Math.random() * 100 + 50;
+		context.beginPath()
+		context.arc(startX, startY, radius, 0, Math.PI*2, false)
+		let r = (Math.random() * 255);
+		let g = (Math.random() * 255);
+		let b = (Math.random() * 255);
+		let color = 'rgb(' + Math.floor(r) + ', ' + Math.floor(g) + ', ' + Math.floor(b) + ')';
+		context.fillStyle = color;
+		context.fill();
+	}
+	// Apply warping
+	var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+	var data = imageData.data;
+	for (let x = 0; x < canvas.width; x++) {
+		for (let y = 0; y < canvas.height; y++) {
+		    let offsetX = Math.sin(y / 6.25) * warp_scale;
+			let offsetY = Math.sin(x / 6.25) * warp_scale;
+			let sourceX = x + offsetX;
+			var sourceY = y + offsetY;
+			if (sourceX >= 0 && sourceX < canvas.width && sourceY >= 0 && sourceY < canvas.height) {
+				let destIndex = (y *  canvas.width + x) * 4;
+				let sourceIndex = (Math.floor(sourceY) * canvas.width + Math.floor(sourceX)) * 4;
+				data[destIndex] = data[sourceIndex]; // R
+				data[destIndex + 1] = data[sourceIndex + 1]; // G
+				data[destIndex + 2] = data[sourceIndex + 2]; // B
+				data[destIndex + 3] = data[sourceIndex + 3]; // A
+			}
+		}
+	}
+	context.putImageData(imageData, 0, 0);
+	var previewImg = document.getElementById("png-preview");
+	previewImg.src = canvas.toDataURL("image/png");
+	previewImg.style.display = "block";
+	var downloadLink = document.getElementById("download-link");
+	downloadLink.href = canvas.toDataURL("image/png");
+	downloadLink.download = "canvas.png"
+	downloadLink.style.display = "block";
+});
